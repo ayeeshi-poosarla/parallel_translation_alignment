8d7bb93520c9e61b0d753c5403bd462f7a5bde45
iunde1@jhu.edu <you@example.com>
2024-11-10 21:40:22 -0500

    python multi-processing code

---
+++ distributed_computing/pymultiprocess.py
@@ -0,0 +1,30 @@
+#start with this why testing and setting up, then move to spark/apache 
+#multiprocessing library lets you run multiple processes concurrently, taking advantage of multiple CPU cores on your computer.
+
+from multiprocessing import Pool 
+
+#multiprocessing creating DNA into six possible reading frames 
+
+def translate_to_proteins(dna_sequence):
+    #however, we decide to do this, put this logic here
+    return [protein_frame1, protein_frame2, protein_frame3, reverse_frame1, reverse_frame2, reverse_frame3]
+
+# List of DNA sequences to process --> getting this from our synthetic data 
+dna_sequences = ["ATCG...", "GCTA...", ...]  
+
+# multiprocessing 
+with Pool() as pool:
+    translated_sequences = pool.map(translate_to_proteins, dna_sequences)
+
+
+#dynamic programming allignment to evaluate allignments in parallel 
+def align_sequences(seq1, seq2):
+    # Implement custom alignment using dynamic programming
+    return alignment_score
+
+# alligning each sequence against all other sequences (or a subset)
+protein_pairs = [(seq1, seq2) for seq1 in translated_sequences for seq2 in translated_sequences]
+
+with Pool() as pool:
+    alignment_scores = pool.starmap(align_sequences, protein_pairs)
+
